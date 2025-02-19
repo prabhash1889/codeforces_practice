2cbc23a154cabaf78b7d41828133b04b7b3f3909
prabhash1889 <prabhashpadhan1889@gmail.com>
2025-02-19 09:46:27 +0530

    1917B c++ done

--- grey/brute-force/1917B.cpp
+++ grey/brute-force/1917B.cpp
@@ -11,5 +11,39 @@ int main()
 
     while (t--)
     {
+        // int n;
+        // cin >> n;
+        // string s;
+        // cin >> s;
+        // vector<int> v;
+        // array<int, 26> cnt{};
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (cnt[s[i] - 'a'] == 0)
+        //     {
+        //         v.push_back(i);
+        //     }
+        //     else
+        //         cnt[s[i] - 'a']++;
+        // }
+        // int64_t ans = 0;
+        // for (int i = 0; i < v.size(); i++)
+        // {
+        //     ans += (n - v[i]);
+        // }
+        // cout << ans << '\n';
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        array<int, 26> cnt{};
+        int dist = 0, ans = 0;
+        for (auto c : s)
+        {
+            dist += cnt[c - 'a']++ == 0;
+            // cout << dist << '\n';
+            ans += dist;
+        }
+        cout << ans << '\n';
     }
 }
